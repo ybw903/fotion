3a92d8a6a1646d6f003a0ed3f37c5d136583a5da
유병우 <byoo@fasoo.com>
2022-08-12 17:48:06 +0900

    feat: retrive props to markdown viewer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import MarkDownViewr from "./componets/MarkDownVierw";
 interface IFile {
   name: string;
   type: string;
   level: number;
+  docs?: string;
   files?: IFile[];
 }
 
 const App = () => {
   const [dirs, setDirs] = useState();
+  const [md, setMD] = useState<undefined | string>();
   useEffect(() => {
     window.api.send("toMain", { type: "GET_DIRS" });
     window.api.receive("fromMain", (data) => {
@@ -20,30 +23,30 @@ const App = () => {
   }, []);
 
   const renderDirs = (dirs: IFile) => {
-    const { name, type, files, level } = dirs;
+    const { name, type, files, level, docs } = dirs;
     return (
-      <>
+      <div className={type}>
         <div
           key={name}
-          className="dir"
+          className={"item"}
           style={{ paddingLeft: `${10 * level}px` }}
+          onClick={() => (type === "file" && docs ? setMD(docs) : undefined)}
         >
           {`${name.substring(name.lastIndexOf("/") + 1)}${
             type === "dir" ? "/" : ""
           }`}
         </div>
-        {/* {type === "dir" && (
-          <div style={{ border: `1px solid gray`, marginBottom: "5px" }}></div>
-        )} */}
         {files && <div>{files.map((file) => renderDirs(file))}</div>}
-      </>
+      </div>
     );
   };
 
   return (
     <div className="App">
-      <div className="leftMenu">{dirs && renderDirs(dirs)}</div>
-      <div className="main"></div>
+      <div className="leftMenu">
+        <div className="menuList">{dirs && renderDirs(dirs)}</div>
+      </div>
+      <div className="main">{md && <MarkDownViewr markdown={md} />}</div>
     </div>
   );
 };
