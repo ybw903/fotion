3d36b5a305703d1d1a55d7432483786a68293a2e
유병우 <conner903@naver.com>
2022-08-13 12:34:56 +0900

    feat: extract directory compoennt

---
+++ src/componets/Directory.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { IoChevronForward } from "react-icons/io5";
+import { IFile } from "../App";
+
+interface DirectoryProps {
+  dirs: IFile;
+  selectedItem: string;
+  handleSelectItem: (dir: IFile) => void;
+}
+
+const Directory = ({
+  dirs,
+  handleSelectItem,
+  selectedItem,
+}: DirectoryProps) => {
+  const { name, type, files, level, docs } = dirs;
+  const [fold, setFold] = useState(false);
+
+  const compositeHandleSelectItem = (dir: IFile) => {
+    handleSelectItem(dir);
+    if (dir.type === "dir") setFold((prev) => !prev);
+  };
+  return (
+    <div className={type}>
+      <div
+        key={name}
+        className={name === selectedItem ? "item-selected" : "item"}
+        style={{ paddingLeft: `${10 * level}px`, cursor: "pointer" }}
+        onClick={() => compositeHandleSelectItem(dirs)}
+      >
+        {type === "dir" && <IoChevronForward />}
+        {`${name.substring(name.lastIndexOf("/") + 1)}`}
+      </div>
+      {files && !fold && (
+        <div>
+          {files.map((file) => (
+            <Directory
+              dirs={file}
+              handleSelectItem={handleSelectItem}
+              selectedItem={selectedItem}
+              key={name}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Directory;
